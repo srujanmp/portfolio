c138af81a85e0177693d82951f664b25552dc94d
Srujan M Padmashali <srujanmpadmashali@gmail.com>
2024-06-05 13:34:43 +0530

    new

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let express=require('express');
 let app=express();
-
+app.use('/',express.static(__dirname+'/src'));
 
 app.get('/',(req,res)=>{
-	res.sendFile(__dirname+'/portfolio.html');
+	res.sendFile(__dirname+'/src/portfolio.html');
 });
 
 app.listen(8000);
