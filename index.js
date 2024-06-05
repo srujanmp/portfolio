ffb93e1ddbb3d580c1ba40fadbf524511ec7ef1a
Srujan M Padmashali <srujanmpadmashali@gmail.com>
2024-06-05 20:18:33 +0530

    updfate 2.0

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 let express=require('express');
 let app=express();
 app.use('/',express.static(__dirname+'/src'));
+app.engine('html',require('ejs').renderFile);
+app.set('view engine','html');
 
-app.get('/',(req,res)=>{
-	res.sendFile(__dirname+'/src/portfolio.html');
+app.get('/',async(req,res)=>{
+	const response=await fetch('https://random-quote-generator-srujan.vercel.app/json');
+  const data=await response.json();
+  res.render(__dirname+'/src/portfolio.html',data);
+  
 });
 
 app.listen(8000);
